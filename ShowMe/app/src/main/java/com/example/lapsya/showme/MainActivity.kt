3d2dd2ec8aee0df100161af391aec26cdd43a3b3
Maria <ilapsya@gmail.com>
2018-03-06 23:28:12 +0300

    day20 - more in-depth app structure

--- ShowMe/app/src/main/java/com/example/lapsya/showme/MainActivity.kt
+++ ShowMe/app/src/main/java/com/example/lapsya/showme/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.lapsya.showme
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    var hello_btn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        hello_btn  = findViewById(R.id.hello_button) as Button
     }
+
+    hello_btn.setOnClickListener = {
+        Toast.makeText(this, "You've pressed me!", Toast.LENGTH_LONG).show()
+    }
+
 }
