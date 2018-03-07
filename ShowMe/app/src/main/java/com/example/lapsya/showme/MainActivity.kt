4b245591dcaa286fb1803a186debab35baf97f23
Maria <ilapsya@gmail.com>
2018-03-07 23:32:50 +0300

    day21 - fix yesterday's 'hello' button

--- ShowMe/app/src/main/java/com/example/lapsya/showme/MainActivity.kt
+++ ShowMe/app/src/main/java/com/example/lapsya/showme/MainActivity.kt
@@ -2,20 +2,21 @@ package com.example.lapsya.showme
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    var hello_btn: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        hello_btn  = findViewById(R.id.hello_button) as Button
+        var hello_btn: Button = findViewById(R.id.hello_button)
     }
 
-    hello_btn.setOnClickListener = {
+    fun hello_btn_click(v: View) {
         Toast.makeText(this, "You've pressed me!", Toast.LENGTH_LONG).show()
     }
 
