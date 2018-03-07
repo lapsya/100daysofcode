a85b47ee7edb91822dca0c25396828ca46fea687
Maria <ilapsya@gmail.com>
2018-03-07 23:45:02 +0300

    day21 - add editText and button interaction

--- ShowMe/app/src/main/java/com/example/lapsya/showme/MainActivity.kt
+++ ShowMe/app/src/main/java/com/example/lapsya/showme/MainActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -13,11 +14,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var hello_btn: Button = findViewById(R.id.hello_button)
     }
 
     fun hello_btn_click(v: View) {
-        Toast.makeText(this, "You've pressed me!", Toast.LENGTH_LONG).show()
+        val input_text: EditText = findViewById(R.id.edit_text_name_input)
+        Toast.makeText(this, input_text.getText(), Toast.LENGTH_LONG).show()
     }
 
 }
