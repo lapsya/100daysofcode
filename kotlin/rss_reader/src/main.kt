6dd17bfe0666afbd36f318b327d52b44b93dfd91
Maria <ilapsya@gmail.com>
2018-04-05 18:30:38 +0300

    day45 - delete old project

--- kotlin/rss_reader/src/main.kt
+++
@@ -1,9 +0,0 @@
-import reader.RSSReader
-
-fun main(args: Array<String>) {
-    println("Hello!")
-    val a = RSSReader("http://androidpolice.com/feed")
-    a.update()
-
-    println(a.feedBuffer?.etag)
-}
